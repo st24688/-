5642ce9b225a12f842a410f293d52668abc24278
Admin <Admin@ADMIN->
2021-10-30 17:02:25 +0300

    Добавьте файлы проекта.

---
+++ Практическая работа/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_работа
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Console.WriteLine("Hello  ")
+        }
+    }
+}
